088e91983d98a2cca209f8b72ad661a76ae2bdff
tonycony <46638919+tonycony@users.noreply.github.com>
2019-06-13 20:24:07 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -127,7 +127,7 @@ if($type == "text"){
 						$sql="SELECT * FROM air_information ORDER BY ID DESC LIMIT 1";//選擇最新的空氣資訊
 						$result=mysqli_query($link,$sql);
 						$row = mysqli_fetch_array($result);
-						$replymessage='您所在區域'."\n"
+						$replymessage='您所在A區'."\n"
 						.'溫度是'.(string)$row['Temperature']."°C\n"
 						.'濕度是'.(string)$row['Humidity']."%\n"
 						.'Co濃度是'.(string)$row['Co']."\n"
@@ -138,7 +138,7 @@ if($type == "text"){
 						$sql="SELECT * FROM b_air_information ORDER BY ID DESC LIMIT 1";//選擇最新的空氣資訊
 						$result=mysqli_query($link,$sql);
 						$row = mysqli_fetch_array($result);
-						$replymessage='您所在區域'."\n"
+						$replymessage='您所在B區'."\n"
 						.'溫度是'.(string)$row['Temperature']."°C\n"
 						.'濕度是'.(string)$row['Humidity']."%\n"
 						.'Co濃度是'.(string)$row['Co'];//回傳給使用者之資訊 \n要用""
