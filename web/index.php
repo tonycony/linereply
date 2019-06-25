2fd3ea32b6bf814b91f896593a5215d3f7653f07
tonycony <46638919+tonycony@users.noreply.github.com>
2019-06-25 19:40:43 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -94,6 +94,8 @@ if($type == "text"){
 			$name=substr($message,7);
 			$sql="UPDATE user set user_name='$name' where user_id='$user_id'";
 			mysqli_query($link,$sql);
+			$sql8="UPDATE user set area='A' WHERE user_id = '$user_id'";
+			mysqli_query($link,$sql8);
 			$post_data = [
 			  "replyToken" => $reply_token,
 			  "messages" => [
@@ -148,7 +150,7 @@ if($type == "text"){
 						.'濕度是'.(string)$row['Humidity']."%\n"
 						.'Co濃度是'.(string)$row['Co'];//回傳給使用者之資訊 \n要用""
 						break;
-					case "NULL":
+					case "C":
 						$sql="SELECT * FROM c_air_information ORDER BY ID DESC LIMIT 1";//選擇最新的空氣資訊
 						$result=mysqli_query($link,$sql);
 						$row = mysqli_fetch_array($result);
