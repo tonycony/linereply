78aa92fab5053752bdd123bc46805b3efcbb1fa7
tonycony <46638919+tonycony@users.noreply.github.com>
2019-08-06 14:42:17 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php
 function push($post_data,$access_token)
 {
-	//fwrite($file, json_encode($post_data)."\n");
 	$ch = curl_init("https://api.line.me/v2/bot/message/reply");
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
@@ -17,6 +16,26 @@ function push($post_data,$access_token)
 	$result = curl_exec($ch);
 	curl_close($ch); 
 }
+function iotpost($count)
+{
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "https://iot.cht.com.tw/iot/v1/device/17944804838/rawdata",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 30,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => "POST",
+	  CURLOPT_POSTFIELDS => "[\r\n  {\r\n    \"id\": \"A\",\r\n    \"save\": true,\r\n    \"value\": [\"$count\"]\r\n  }\r\n]",
+	  CURLOPT_HTTPHEADER => array(
+	    "CK: DK7UCUHT1HB5BB0G71",
+	    "Content-Type: application/json",
+	  ),
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+}
 include("mysql_connect.inc.php");
 $access_token ='yWARnZrlhZ0gEqjA7h3kZEOIaaxTndaMIYdLh1kD/RQY0w10Jq9PH6mn5P0lKRBRsokFk7LfoUrOqii3yoERK9uldJLEEqQK0EtRHE3ug/5iNEGBkTi7+QJjIJALp2QUiC6FvMo6nkvDuU+lwsVxVgdB04t89/1O/w1cDnyilFU=';
 $json_string = file_get_contents('php://input');
@@ -28,7 +47,10 @@ $user_id  = $event->{"source"}->{"userId"};
 $reply_token = $event->{"replyToken"};
 date_default_timezone_set('Asia/Taipei');
 $Time=date("Y-m-d H:i:s") ;
+$countA=0;
 if('012b789221' == $event->beacon->hwid && 'enter'==$event->beacon->type){
+	$countA++;
+	iotpost($countA);
 	$sql6="insert into history_list(user_id,process_area,time) values ('$user_id','A','$Time')";
 	mysqli_query($link,$sql6);
 	$sql8="UPDATE user set area='A' WHERE user_id = '$user_id'";
@@ -55,6 +77,8 @@ if('012b789221' == $event->beacon->hwid && 'enter'==$event->beacon->type){
 	push($post_data,$access_token);
 }
 if('012b789221' == $event->beacon->hwid && 'leave'==$event->beacon->type){
+	$countA--;
+	iotpost($countA);
 	$time1=$Time;
 	$sql="SELECT time FROM history_list WHERE user_id = '$user_id' && process_area='A' ORDER BY ID DESC LIMIT 1";
 	$result = mysqli_query($link,$sql);
