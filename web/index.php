ade1e3a63f28268312d7f39ad0e9afe2316829f1
tonycony <46638919+tonycony@users.noreply.github.com>
2020-06-04 21:51:51 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -27,16 +27,24 @@ $user_id  = $event->{"source"}->{"userId"};
 $reply_token = $event->{"replyToken"};
 date_default_timezone_set('Asia/Taipei');
 if($type == "text"){
+	$sql="SELECT * FROM air_information ORDER BY ID DESC LIMIT 1";//選擇最新的空氣資訊
+	$result=mysqli_query($link,$sql);
+	$row = mysqli_fetch_array($result);
+	$replymessage='歡迎！'."\n"
+	.'提供您空氣品質資訊'."\n"
+	.'溫度是'.(string)$row['Temperature']."°C\n"
+	.'濕度是'.(string)$row['Humidity']."%\n"
+	.'Co濃度是'.(string)$row['Co']."\n";//回傳給使用者之資訊 \n要用""
 	$post_data = [
 	  "replyToken" => $reply_token,
 	  "messages" => [
 		[
 		  "type" => "text",
-		  "text" => $message
+		  "text" => $replymessage
 		]
 	  ]
 	];
-    push($post_data,$access_token);
+    	push($post_data,$access_token);
 }
 if('013e6460ee' == $event->beacon->hwid && 'enter'==$event->beacon->type){
 	$post_data = [
